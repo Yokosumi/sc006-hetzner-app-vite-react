b7cd41d89be262d489a30e2aa7b295d8b654208a
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-02-14 15:37:53 +0100

    small change

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -14,7 +14,7 @@ export const PageWelcome = () => {
 				This site has <strong>{jobs.length}</strong> jobs and{' '}
 				<strong>{skills.length}</strong> skills.
 			</p>
-			<p>Test Update 3</p>
+			<p>Test Update 4</p>
 		</div>
 	);
 };
